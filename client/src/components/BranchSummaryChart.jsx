1098915cb73f4161914dc8c56cf72bc17e1dfe22
devdemon713 <kolheprashant456@gmail.com>
2026-09-12 15:07:10 +0530

    acap

--- client/src/components/BranchSummaryChart.jsx
+++ client/src/components/BranchSummaryChart.jsx
@@ -48,7 +48,7 @@ function BranchSummaryChart({ branches, flashId, vacantOnly = false }) {
               background: '#FFFF00', color: '#000', fontWeight: 800, fontSize: '13px',
               padding: '5px 16px', borderRadius: '3px', border: '1.5px solid #ccc', whiteSpace: 'nowrap'
             }}>
-              Total Vacant CAP Seats: {grandTotal}
+              ALL INDIA SEAT FOR ACAP ROUND: {grandTotal}
             </div>
           </div>
         </div>
